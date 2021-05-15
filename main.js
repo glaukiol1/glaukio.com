7cd531b18ee753a7347980cca6f49f71489117f1
glaukiol1 <glaukiol1@gmail.com>
2021-05-16 00:02:41 +0200

    Major changes

--- main.js
+++ main.js
@@ -26,4 +26,35 @@ nextproject.addEventListener('click', (ev) => {
         }
     }
 })
+function doScrolling(elementY, duration) { 
+    var startingY = window.pageYOffset;
+    var diff = elementY - startingY;
+    var start;
+  
+    // Bootstrap our animation - it will get called right before next frame shall be rendered.
+    window.requestAnimationFrame(function step(timestamp) {
+      if (!start) start = timestamp;
+      // Elapsed milliseconds since start of scrolling.
+      var time = timestamp - start;
+      // Get percent of completion in range [0, 1].
+      var percent = Math.min(time / duration, 1);
+  
+      window.scrollTo(0, startingY + diff * percent);
+  
+      // Proceed with animation as long as we wanted it to.
+      if (time < duration) {
+        window.requestAnimationFrame(step);
+      }
+    })
+  }
+  
+
+document.getElementById('make-scroll').addEventListener('click', (e)=>{
+    document.body.style.overflowY = 'scroll';
+    main2.classList = 'fade-out';
+    setTimeout( () => {
+        main2.style.opacity = '0'
+        doScrolling(document.getElementById('scroll-div').getBoundingClientRect().top, 2500)
+    },1000)
+})
 
